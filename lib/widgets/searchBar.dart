ae47c738c6d3f288088b8314553a9d8db3339461
Prashidha0O1 <89299485+Prashidha0O1@users.noreply.github.com>
2023-07-06 08:53:08 +0545

    First Commit

---
+++ lib/widgets/searchBar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:monkey_app_demo/const/colors.dart';
+import 'package:monkey_app_demo/utils/helper.dart';
+
+class SearchBar extends StatelessWidget {
+  final String title;
+  // ignore: non_constant_identifier_names
+  SearchBar({required this.title, required String Title});
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Container(
+        height: 50,
+        width: double.infinity,
+        decoration: ShapeDecoration(
+          shape: StadiumBorder(),
+          color: AppColor.placeholderBg,
+        ),
+        child: TextField(
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            prefixIcon: Image.asset(
+              Helper.getAssetName("search_filled.png", "virtual"),
+            ),
+            hintText: title,
+            hintStyle: TextStyle(
+              color: AppColor.placeholder,
+              fontSize: 18,
+            ),
+            contentPadding: const EdgeInsets.only(
+              top: 17,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
